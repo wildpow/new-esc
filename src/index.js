e6facd995773142beb853d4a0e5ce48ce605b514
Aaron Wilder <element1028@gmail.com>
2018-01-17 20:25:25 -0800

    wired up Apollo, Graphql, Router, and Styled-components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo'; 
+
+import App from './App';
+
+const GRAPHCMS_API = 'https://api.graphcms.com/simple/v1/poop';
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: GRAPHCMS_API }),
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <BrowserRouter>
+    <ApolloProvider client={ client } >
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
+  , document.getElementById('root'));
+
