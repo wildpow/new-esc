93cab8fd38585eb65b6d1d16809f1bce88758a8a
Aaron Wilder <element1028@gmail.com>
2018-01-27 23:12:11 -0800

    working on single base

--- src/Components/Adjustable/SingleBase.js
+++ src/Components/Adjustable/SingleBase.js
@@ -1,11 +1,71 @@
 import React from 'react'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo';
+import Markdown from 'react-markdown'
+import '../../app.css';
+import styled from 'styled-components';
 
+const Wrapper = styled.div`
+display: flex;
+flex-direction: column;
+border: 2px solid black;
+justify-content: center;
+`;
+const MainTitle = styled.header`
+font-family: 'Roboto', sans-serif;
+font-size: 1.2rem;
+padding: 0px 30px 0px 30px;
+background-color: #1565c0;
+color: white;
+text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
 
-const SingleBase = () => {
-  return (
-    <div>
-      single base
-    </div>
-  )
+`;
+const SingleBase = ({data: { loading, error, base}}) => {
+  if (error) return <h1>Error fetching the base</h1>
+  if (!loading) {
+    return(
+      <Wrapper className="animated fadeIn">
+        <MainTitle>
+          <h1>{base.name}</h1>
+        </MainTitle>
+        <div>
+
+        </div>
+
+      </Wrapper>
+    )
+  }
+  return <h3>loading base</h3>
 }
-export default SingleBase
+
+export const singleBase = gql`
+query singleBase($id: ID!) {
+  base: AdjBases(id: $id) {
+    id
+    fullName
+    keyfeatures
+    features
+    price
+    brandLine
+    brandName
+    baseDescription
+    height
+    coverImg {
+      handle
+    }
+    detail1 {
+      handle
+    }
+    detail2 {
+      handle
+    }
+  }
+}
+`
+export default graphql(singleBase, {
+  options: ({ match }) => ({
+    variables: {
+      id: match.params.id
+    }
+  })
+})(ingleBase)
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo'; 
-// import { persistCache } from 'apollo-cache-persist';
+import { persistCache } from 'apollo-cache-persist';
 
 import App from './App';
 
@@ -25,10 +25,10 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
   connectToDevTools: true
 });
-// persistCache({
-//   cache: client.cache,
-//   storage: window.localStorage
-// })
+persistCache({
+  cache: client.cache,
+  storage: window.localStorage
+})
 
 ReactDOM.render(
   <BrowserRouter>
