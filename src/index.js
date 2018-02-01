6636a644522e53c7f877e8d4f6055ffae335df49
Aaron Wilder <element1028@gmail.com>
2018-02-01 10:58:32 -0800

    google analytics up and running

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
-//import { BrowserRouter } from 'react-router-dom';
+
 import WebFont from 'webfontloader';
 
 import { ApolloClient } from 'apollo-client';
