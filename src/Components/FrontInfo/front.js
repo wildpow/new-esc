15fd37308a8c5efed05e7d6b92ac4436c1fa2b4a
wildpow <element1028@gmail.com>
2018-09-17 12:48:18 -0700

    typo

--- src/Components/FrontInfo/front.js
+++ src/Components/FrontInfo/front.js
@@ -7,7 +7,7 @@ const Front = () => {
     <Wrapper>
       <Headline>The Real Deal</Headline>
       <P>
-      The Real Deal is going on now, save up to $1000 on popular mattress sets! Get up to a $300 Gift with purchase of a new Tempur-Pedic mattress.  Upgrade to the highest rated luxury innerspring and save up to $1000 on Stearns & Foster Adjustable mattress sets.  Save up to $200 on top-rated Sealy Hybrid and Conform mattress models.  The biggest mattress sale of the year is going on now with The Real Deal now through September 10th, so stop by today and save!
+      The Real Deal is going on now, save up to $1000 on popular mattress sets! Get up to a $300 Gift with purchase of a new Tempur-Pedic mattress.  Upgrade to the highest rated luxury innerspring and save up to $1000 on Stearns & Foster Adjustable mattress sets.  Save up to $200 on top-rated Sealy Hybrid and Conform mattress models.  The biggest mattress sale of the year is going on now with The Real Deal now through September 17th, so stop by today and save!
 …
       </P>
       <Headline red>“Sleep Like the Experts Do!”</Headline>
